21ca6c66ef8ad67d9669b18403b0d69614f06b4f
FazalSyed01 <fazalsyed601@gmail.com>
2023-06-16 00:08:22 +0500

    First Commit

---
+++ javascript.js
@@ -0,0 +1,24 @@
+let tiles = document.querySelectorAll(".item");
+let x = 0;
+// console.log(tiles[0]);
+for (let i = 0; i < tiles.length; i++) {
+  tiles[i].addEventListener(
+    "click",
+    function changeImage(e) {
+      console.log("hello");
+      const img = document.createElement("img");
+      if (x === 0 || x === 2 || x === 4 || x === 6 || x === 8) {
+        img.src = "imgs/X.png";
+      } else {
+        img.src = "imgs/O.png";
+      }
+      img.style.width = "100%";
+      img.style.height = "100%";
+      img.style.margin = "0px";
+      img.style.padding = "0px";
+      tiles[i].appendChild(img);
+      x += 1;
+    },
+    { once: true }
+  );
+}
