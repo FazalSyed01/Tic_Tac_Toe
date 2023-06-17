b5e0ae253c197a4449e543de5eb444686d461183
FazalSyed01 <fazalsyed601@gmail.com>
2023-06-17 12:44:23 +0500

    qwerty

--- javascript.js
+++ javascript.js
@@ -5,7 +5,8 @@ for (let i = 0; i < tiles.length; i++) {
   tiles[i].addEventListener(
     "click",
     function changeImage(e) {
-      console.log("hello");
+      console.log(tiles[i]);
+
       const img = document.createElement("img");
       if (x === 0 || x === 2 || x === 4 || x === 6 || x === 8) {
         img.src = "imgs/X.png";
@@ -18,6 +19,12 @@ for (let i = 0; i < tiles.length; i++) {
       img.style.padding = "0px";
       tiles[i].appendChild(img);
       x += 1;
+      if (
+        (x === 0 || x === 2 || x === 4 || x === 6 || x === 8) &&
+        tiles[i] === 0
+      ) {
+        console.log("Image is X");
+      }
     },
     { once: true }
   );
